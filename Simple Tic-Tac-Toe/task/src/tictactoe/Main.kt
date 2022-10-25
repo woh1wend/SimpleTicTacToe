b82d956d507ab5c7e9e144437af84da61b248e58
Wohlwend <michaeldias84@gmail.com>
2022-10-25 09:45:10 +0100

    Result not parsing. But I'm keeping the code anyway and try something else.

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -17,46 +17,62 @@ class Tictactoe constructor (private val _play: List<Char>) {
         println("| ${matrix()[i].joinToString(" ")} |")
         }
         println("---------")
+
+        result()
     }
 
-    fun result() {
-        when {
-            horizontal() -> println("is horizontal")
-            vertical() -> println("is vertical")
-            diagonal() -> println("is diagonal")
-            else -> println("is false")
+    private fun result () {
+        when (true) {
+            notFinished() -> println("Game not finished")
+            !countPlays() -> println("Impossible")
+            ((horizontal() xor vertical() || horizontal() xor diagonal() || vertical() xor diagonal())
+                    && countPlays() && (_play.contains('_') || '_' !in _play)) -> check()
         }
+    }
+    private fun check() {
+        when (true) {
+            horizontal() -> checkH()
+            vertical() -> checkV()
+            diagonal() -> checkD()
 
+        }
     }
 
-    private fun horizontal(): Boolean {
+    private fun checkH() {
+        val listH = mutableListOf<List<Char>>()
         for (row in matrix().indices) {
-            if (matrix()[row].all {it == 'X'} || matrix()[row].all {it == 'O'} ) {
-                return true
+            if (matrix()[row].all { it == 'X' } || matrix()[row].all { it == 'O' }) {
+                listH.add(matrix()[row])
             }
         }
-        return false
+        if (listH.size < 2) {
+            println("${listH[0][0]} wins")
+        }
     }
 
-    private fun vertical(): Boolean {
+    private fun checkV() {
         val list = mutableListOf<Char>()
+        val listV = mutableListOf<List<Char>>()
         for (row in matrix().indices) {
             for (column in matrix().indices) {
-                 list.add(matrix()[column][row])
+                list.add(matrix()[column][row])
             }
         }
         for (i in list.chunked(3).indices) {
             if (list.chunked(3)[i].all {it == 'X'} || list.chunked(3)[i].all {it == 'O'}) {
-                return true
+                listV.add(list.chunked(3)[i])
             }
         }
-        return false
+        if (listV.size < 2) {
+            println("${listV[0][0]} wins")
+        }
     }
 
-    private fun diagonal(): Boolean {
+    private fun checkD() {
         val list1 = mutableListOf<Char>()
         val list2 = mutableListOf<Char>()
-        val diagonaList = listOf(list1, list2)
+        val diagonal = listOf(list1, list2)
+        val listD: MutableList <List<Char>> = mutableListOf()
 
         for (row in matrix().indices) {
             for (column in matrix().indices) {
@@ -74,17 +90,101 @@ class Tictactoe constructor (private val _play: List<Char>) {
             }
         }
 
-        for (i in diagonaList.indices) {
-            if (diagonaList[i].all { it == 'X' } || diagonaList[i].all { it == 'O' }) {
-                return true
+        for (i in diagonal.indices) {
+            if (diagonal[i].all { it == 'X' } || diagonal[i].all { it == 'O' }) {
+                listD.add(diagonal[i])
             }
+        }
+
+        if (listD.size < 2) {
+            println("${listD[0][0]} wins")
+        }
+
+    }
+
+    private fun horizontal(): Boolean {
+        val listH = mutableListOf<List<Char>>()
+        for (row in matrix().indices) {
+            if (matrix()[row].all {it == 'X'} || matrix()[row].all {it == 'O'}) {
+                listH.add(matrix()[row])
+            }
+        }
+        if (listH.size < 2) {
+            return true
+
+        }
+
+        return false
+    }
+
+    private fun vertical(): Boolean {
+        val list = mutableListOf<Char>()
+        val listV = mutableListOf<List<Char>>()
+        for (row in matrix().indices) {
+            for (column in matrix().indices) {
+                 list.add(matrix()[column][row])
+            }
+        }
+        for (i in list.chunked(3).indices) {
+            if (list.chunked(3)[i].all {it == 'X'} || list.chunked(3)[i].all {it == 'O'}) {
+                listV.add(list.chunked(3)[i])
+            }
+        }
+        if (listV.size < 2) {
+            return true
         }
         return false
     }
 
-    private fun draw(){}
+    private fun diagonal(): Boolean {
+        val list1 = mutableListOf<Char>()
+        val list2 = mutableListOf<Char>()
+        val diagonal = listOf(list1, list2)
+        val listD: MutableList <List<Char>> = mutableListOf()
 
-    private fun impossible() {}
+        for (row in matrix().indices) {
+            for (column in matrix().indices) {
+                if (row + column == matrix().size - 1) {
+                    list1.add(matrix()[row][column])
+                }
+            }
+        }
+
+        for (row in matrix().indices) {
+            for (column in matrix().indices) {
+                if (row == column) {
+                    list2.add(matrix()[row][column])
+                }
+            }
+        }
+
+        for (i in diagonal.indices) {
+            if (diagonal[i].all { it == 'X' } || diagonal[i].all { it == 'O' }) {
+                listD.add(diagonal[i])
+            }
+        }
+
+        if (listD.size < 2) {
+            return true
+        }
+        return false
+    }
+
+    private fun notFinished(): Boolean {
+        if (_play.contains('_') && !horizontal() && !vertical() && !diagonal()) {
+            return true
+        }
+        return false
+    }
+
+    private fun countPlays(): Boolean {
+        val x = _play.count {it == 'X'}
+        val o = _play.count {it == 'O'}
+        if (x - o < 2 || o - x < 2) {
+            return true
+        }
+        return false
+    }
 
 }
 
@@ -92,7 +192,6 @@ fun main() {
 
     val play  = Tictactoe(readln().uppercase().toList())
     play.gamePrint()
-    play.result()
 
 
 }
